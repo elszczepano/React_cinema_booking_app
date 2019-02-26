486d317f91ab6c0848015ea3b73a036a64d40089
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-26 19:20:49 +0100

    Setup theme provider.

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { theme } from '../utils/theme';
+
+const GlobalStyle = createGlobalStyle`
+	body {
+		margin: 0;
+		padding: 0;
+		background-color: black;
+		color: white;
+		font-family: 'Lato', sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
+`;
+
+const Layout = ({children}) => {
+	return (
+		<ThemeProvider theme={theme}>
+			<>
+				<GlobalStyle />
+				{children}
+			</>
+		</ThemeProvider>
+	);
+};
+
+export default Layout;
