63c1859e580bbbab6add657b0cb7b4a46262ab71
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-27 19:49:16 +0100

    Add link to a single movie. Add more CSS.

--- src/Components/AvailableMovies/SingleMovieShortcut.jsx
+++ src/Components/AvailableMovies/SingleMovieShortcut.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Container = styled.div`
+const Container = styled(Link)`
+	text-decoration: none;
+	color: white;
 	display: grid;
 	padding: 20px 0;
 	border-bottom: 2px solid orange;
+	transition: .5s all;
+	cursor: pointer;
+	&:hover {
+		color: black;
+		background-color: orange;
+		transform: scale(1.03);
+	
+	}
 `;
 const Title = styled.h2`
 	margin: 10px 0;
@@ -18,17 +29,17 @@ const Details = styled.ul`
 const Detail = styled.li`
 	margin: 0 5px;
 	&:first-of-type {
-		margin: 0;
+		margin: 0 5px 0 0 ;
 	}
 `;
 const SingleMovieShortcut = ({movie}) => {
 	return (
-		<Container>
+		<Container to={`/movie/${movie.id}`}>
 			<Title>{movie.title}</Title>
 			<Details>
 				{movie.categories.map((category, i) => <Detail key={i}>{category},</Detail>)}
-				<Detail>Age limit: {movie.ageLimit},</Detail>
-				<Detail>Duration: {movie.duration} min.</Detail>
+				<Detail>Przedział wiekowy: {movie.ageLimit},</Detail>
+				<Detail>Czas trwania: {movie.duration} min.</Detail>
 			</Details>
 		</Container>
 	);
