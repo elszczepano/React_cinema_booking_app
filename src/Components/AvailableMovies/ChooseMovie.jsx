8619cf0dd4c5de076859080fefa19ee1d7f76b60
Dominik <szczepaniak.dominik98@gmail.com>
2019-02-28 21:00:14 +0100

    Pass the title of movie to the next step.

--- src/Components/AvailableMovies/ChooseMovie.jsx
+++ src/Components/AvailableMovies/ChooseMovie.jsx
@@ -20,22 +20,30 @@ const H3 = styled.h3`
 	color: orange;
 `;
 
-
 class ChooseMovie extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			movieId: 0
+			movieId: 0,
+			movieTitle: ''
 		};
 	}
 	chooseMovie = e => {
+		const title = this.props.movies.filter(movie => movie.id === parseInt(e.target.value))[0].title;
+		console.log(title);
 		this.setState({
+			movieTitle: title,
 			movieId: e.target.value
 		});
 	};
 	render() {
 		return (
-			this.state.movieId ? <Redirect to={`/seats/${this.state.movieId}`} /> :
+			this.state.movieId ? <Redirect to={{
+				pathname: `/seats/${this.state.movieId}`,
+				state: {
+					title: this.state.movieTitle
+				}
+			}} /> :
 				<Wrapper>
 					<H3>Wybierz film:</H3>
 					<Select onChange={this.chooseMovie} name="chooseMovie" id="chooseMovie">
