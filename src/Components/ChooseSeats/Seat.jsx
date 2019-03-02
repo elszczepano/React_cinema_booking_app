9d8dc65837bf5000599cc346578d3db3f980b869
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-02 20:51:18 +0100

    Rewrite component to a class component | Handle changing status and color | Add some styles and hide input's.

--- src/Components/ChooseSeats/Seat.jsx
+++ src/Components/ChooseSeats/Seat.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 
 const Input = styled.input`
-
+	display: none;
 `;
 const Label = styled.label`
-	width: 15px;
-	height: 15px;
-	background-color: white;
+	width: 25px;
+	height: 25px;
 `;
 
-const Seat = ({row, seat, seatId}) => {
-	return (
-		<>
-			<Label htmlFor={seatId}></Label>
-			<Input type="checkbox" id={seatId} name={seatId}/>
-		</>
-	);
-};
+class Seat extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			backgroundColor: parseInt(this.props.available) ? 'red' : 'white'
+		};
+	}
+	changeStatus = () => {
+		if(!parseInt(this.props.available)) {
+			this.state.backgroundColor === 'white' ? this.setState({backgroundColor: 'green'}) : this.setState({backgroundColor: 'white'});
+			this.props.toggleSeat(this.props.row, this.props.seat);
+		}
+	};
+	render() {
+		const {seatId, available} = this.props;
+		return (
+			<>
+				<Label style={{backgroundColor: this.state.backgroundColor}} onClick={this.changeStatus} available={available} htmlFor={seatId}></Label>
+				<Input type="checkbox" id={seatId} name={seatId} disabled={available}/>
+			</>
+		);
+	}
+}
 
 export default Seat;
