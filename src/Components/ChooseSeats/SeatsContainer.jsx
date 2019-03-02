1c0b45d9e553cbbdc935d2b26ac5c0e138b73cd8
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-02 20:52:12 +0100

    Create method for choosing seats. Add some CSS styles.

--- src/Components/ChooseSeats/SeatsContainer.jsx
+++ src/Components/ChooseSeats/SeatsContainer.jsx
@@ -4,17 +4,19 @@ import API from '../../apiConfig';
 import Seat from './Seat';
 
 const Grid = styled.div`
+	margin-top: 100px;
 	display: grid;
 	grid-template-rows: repeat(14, 1fr);
 	grid-template-columns: repeat(20, 1fr);
-	grid-gap: 2px;
+	grid-gap: 20px;
 `;
 
 class SeatsContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			availableSeats: []
+			availableSeats: [],
+			choosedSeats: []
 		};
 	}
 	componentDidMount() {
@@ -24,11 +26,28 @@ class SeatsContainer extends Component {
 			});
 		});
 	}
+	toggleSeat = (row, seat) => {
+		if(this.state.choosedSeats.length) {
+			const previousArrayLenght = this.state.choosedSeats.length;
+			const updatedArray = this.state.choosedSeats.filter(elem => !(elem.row === row && elem.seat === seat));
+			this.setState({
+				choosedSeats: [...updatedArray]
+			});
+			console.log(updatedArray.length !== previousArrayLenght);
+			if(updatedArray.length !== previousArrayLenght) return;
+		}
+		this.setState({
+			choosedSeats: [...this.state.choosedSeats,{
+				row: row,
+				seat: seat
+			}]
+		});
+	};
 	render() {
 		return (
 			<Grid>
 				{this.state.availableSeats ? this.state.availableSeats.map((row, rowIndex) =>
-					row.seats.map((seat, seatIndex) => <Seat seatId={`${rowIndex}${seatIndex}`} key={`${rowIndex}${seatIndex}`} row={rowIndex} seat={seatIndex} available={seat}/>)
+					row.seats.map((seat, seatIndex) => <Seat toggleSeat={this.toggleSeat} seatId={`${rowIndex}${seatIndex}`} key={`${rowIndex}${seatIndex}`} row={rowIndex} seat={seatIndex} available={seat}/>)
 				): null}
 			</Grid>
 		);
