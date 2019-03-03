04a5b3b4006ec042ef293ca0fb39c636f4c9a09b
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-03 21:47:19 +0100

    Move function to the parent.

--- src/Components/ChooseSeats/SeatsContainer.jsx
+++ src/Components/ChooseSeats/SeatsContainer.jsx
@@ -4,19 +4,19 @@ import API from '../../apiConfig';
 import Seat from './Seat';
 
 const Grid = styled.div`
-	margin-top: 100px;
+	margin-top: 30px;
 	display: grid;
 	grid-template-rows: repeat(14, 1fr);
 	grid-template-columns: repeat(20, 1fr);
 	grid-gap: 20px;
+	justify-items: center;
 `;
 
 class SeatsContainer extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			availableSeats: [],
-			choosedSeats: []
+			availableSeats: []
 		};
 	}
 	componentDidMount() {
@@ -24,30 +24,16 @@ class SeatsContainer extends Component {
 			this.setState({
 				availableSeats: res.data
 			});
+		}).catch(() => {
+			window.alert('Nie udało się pobrać listy dostępnych miejsc. Spróbuj jeszcze raz.');
 		});
 	}
-	toggleSeat = (row, seat) => {
-		if(this.state.choosedSeats.length) {
-			const previousArrayLenght = this.state.choosedSeats.length;
-			const updatedArray = this.state.choosedSeats.filter(elem => !(elem.row === row && elem.seat === seat));
-			this.setState({
-				choosedSeats: [...updatedArray]
-			});
-			console.log(updatedArray.length !== previousArrayLenght);
-			if(updatedArray.length !== previousArrayLenght) return;
-		}
-		this.setState({
-			choosedSeats: [...this.state.choosedSeats,{
-				row: row,
-				seat: seat
-			}]
-		});
-	};
+
 	render() {
 		return (
 			<Grid>
 				{this.state.availableSeats ? this.state.availableSeats.map((row, rowIndex) =>
-					row.seats.map((seat, seatIndex) => <Seat toggleSeat={this.toggleSeat} seatId={`${rowIndex}${seatIndex}`} key={`${rowIndex}${seatIndex}`} row={rowIndex} seat={seatIndex} available={seat}/>)
+					row.seats.map((seat, seatIndex) => <Seat toggleSeat={this.props.toggleSeat} seatId={`${rowIndex}${seatIndex}`} key={`${rowIndex}${seatIndex}`} row={rowIndex} seat={seatIndex} available={seat}/>)
 				): null}
 			</Grid>
 		);
