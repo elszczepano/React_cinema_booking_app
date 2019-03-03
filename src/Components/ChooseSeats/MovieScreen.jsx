49a48034b83c542aa39a9023d13f25c7067d1212
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-03 21:42:12 +0100

    Add movie screen for preview.

---
+++ src/Components/ChooseSeats/MovieScreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Screen = styled.div`
+	margin-top: 30px;
+	width: 100%;
+	background-color: white;
+	color: black;
+	text-align: center;
+	padding: 3px 0;
+	font-size: 14px;
+`;
+
+const MovieScreen = () => {
+	return (
+		<Screen>Ekran</Screen>
+	);
+};
+
+export default MovieScreen;
--- src/Containers/ChooseSeats.jsx
+++ src/Containers/ChooseSeats.jsx
@@ -1,8 +1,12 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import { Redirect } from 'react-router';
 import Layout from '../layout/Layout';
 import ChoosedMovieTitle from '../Components/ChooseSeats/ChoosedMovieTitle';
 import SeatsContainer from '../Components/ChooseSeats/SeatsContainer';
+import ConfirmChoose from '../Components/ChooseSeats/ConfirmChoose';
+import API from '../apiConfig';
+import MovieScreen from '../Components/ChooseSeats/MovieScreen';
 
 const Container = styled.div`
 	max-width: 1000px;
@@ -10,15 +14,65 @@ const Container = styled.div`
 `;
 
 class ChooseSeats extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			choseSeats: [],
+			paymentToken: ''
+		};
+	};
+	toggleSeat = (row, seat) => {
+		if(this.state.choseSeats.length) {
+			const previousArrayLenght = this.state.choseSeats.length;
+			const updatedArray = this.state.choseSeats.filter(elem => !(elem.row === row && elem.seatNumber === seat));
+			this.setState({
+				choseSeats: [...updatedArray]
+			});
+			if(updatedArray.length !== previousArrayLenght) return;
+		}
+		this.setState({
+			choseSeats: [...this.state.choseSeats,{
+				row: row,
+				seatNumber: seat
+			}]
+		});
+	};
+	sendReservationRequest = () => {
+		if(!this.state.choseSeats.length) {
+			window.alert('Nie wybrano miejsc na seans. Proszę wybrać co najmniej jedno miejsce');
+			return;
+		} else {
+			const confirm = window.confirm('Czy jesteś pewien, że chcesz dokonać rezerwacji wybranych miejsc?');
+			if(confirm) {
+				API.post('Reservation/ReserveSeats',{
+					showingId: this.props.match.id,
+					seats: [...this.state.choseSeats]
+				}).then(response => this.setState({paymentToken: response.data}))
+					.catch(() => {
+						window.alert('Coś poszło nie tak. Proszę spróbuj jeszcze raz.');
+					});
+			}
+		}
+
+	};
 	render() {
 		const title = !this.props.location.state ? '' : this.props.location.state.title;
 		return (
-			<Layout>
-				<Container>
-					<ChoosedMovieTitle title={title}/>
-					<SeatsContainer showingId={this.props.match.params.id}/>
-				</Container>
-			</Layout>
+			this.state.paymentToken ? <Redirect to={{
+				pathname: '/payment',
+				state: {
+					token: this.state.paymentToken,
+					title: title
+				}
+			}} /> :
+				<Layout>
+					<Container>
+						<ChoosedMovieTitle title={title}/>
+						<MovieScreen/>
+						<SeatsContainer toggleSeat={this.toggleSeat} showingId={this.props.match.params.id}/>
+						<ConfirmChoose sendReservationRequest={this.sendReservationRequest}/>
+					</Container>
+				</Layout>
 		);
 	}
 }
