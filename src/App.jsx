2969cef63c196950379f083a110deac8d199ad53
Dominik <szczepaniak.dominik98@gmail.com>
2019-03-04 18:13:19 +0100

    Update basename.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AvailableMovies from './Containers/AvailableMovies';
 import Movie from './Containers/Movie';
 import ChooseSeats from './Containers/ChooseSeats';
 import Payment from './Containers/Payment';
 
+const basename = window.location.hostname === 'localhost' ? 'localhost' : 'https://elszczepano.github.io/ARPIdeas_recruitment/';
+
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<>
+			<Router basename={basename}>
+				<Switch>
 					<Route exact path="/" component={AvailableMovies} />
 					<Route exact path="/movie/:id" component={Movie} />
 					<Route exact path="/seats/:id" component={ChooseSeats} />
 					<Route exact path="/payment" component={Payment} />
-				</>
+				</Switch>
 			</Router>
 		);
 	}
